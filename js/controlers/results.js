035f053819203a7880d4a0b195cd84f9f21b9dc3
Zbigniew Stasiak <stasiakzb@gmail.com>
2017-10-03 23:46:28 +0200

    1.0.2

--- js/controlers/results.js
+++ js/controlers/results.js
@@ -4,13 +4,23 @@
         .module("KalistenikaApp")
         .controller("resultsCtrl", ResultsController);
     
-    ResultsController.$inject = ['quizMetrics'];
+    ResultsController.$inject = ['quizMetrics', 'DataService'];
     
-    function ResultsController(quizMetrics){
+    function ResultsController(quizMetrics, DataService){
         var vm = this;
         
         vm.quizMetrics = quizMetrics;
+        vm.DataService = DataService;
+        vm.activeQuestion = 0;
+        vm.getAnswerClass = getAnswerClass;
         
+        function getAnswerClass(index){
+            if(index = quizMetrics.correctAnswers[vm.activeQuestion]){
+                return "bg-success";   
+            } else if(index === DataService.quizQuestions[vm.activeQuestion].selected){
+                return "bg-danger";   
+            }
+        }
     }
     
 })();
